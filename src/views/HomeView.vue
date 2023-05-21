17f95efc39995400ed4b0d6aa6d64705118ea6f1
SagbumWoo <sbwoo87@gmail.com>
2023-05-21 21:05:56 +0900

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,26 @@
 <template>
   <main>
-    <p>home</p>
+    <p>home 2</p>
     <p>{{ counterStore.count }}</p>
     <div>
       <button @click="counterStore.decrement">-</button>
       <button @click="counterStore.increment">+</button>
     </div>
     <p>{{ counterStore.evenOrOdd }}</p>
-    <hr>
+    <hr />
     <button @click="todoStore.getAllTodos">getAllTodos</button>
     <button @click="todoStore.clearTodos">clearTodos</button>
     <!-- <div>{{ counterStore.todoList }}</div> -->
     <ul>
-      <li v-for="todo in todoStore.todoList" :key="todo.id">{{ todo.title }} </li>
+      <li v-for="todo in todoStore.todoList" :key="todo.id">{{ todo.title }}</li>
     </ul>
-
   </main>
 </template>
 
 <script setup>
 import { useCounterStore } from '@/stores/counter'
-import { useTodoStore} from '@/stores/todo'
+import { useTodoStore } from '@/stores/todo'
 
 const counterStore = useCounterStore()
-const todoStore = useTodoStore();
+const todoStore = useTodoStore()
 </script>
